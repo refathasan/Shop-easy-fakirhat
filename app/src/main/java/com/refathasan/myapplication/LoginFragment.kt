143913397316ac88c06dab12f088d5ed752fe2b8
refat hasan <refat.aiub@hotmail.com>
2023-05-16 05:18:23 +0530

    version 1.0.0.0 commit number 1.03 - Registration & login process complete

--- app/src/main/java/com/refathasan/myapplication/LoginFragment.kt
+++ app/src/main/java/com/refathasan/myapplication/LoginFragment.kt
@@ -7,20 +7,8 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [LoginFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class LoginFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
--- app/src/main/java/com/refathasan/myapplication/RegisterFragment.kt
+++ app/src/main/java/com/refathasan/myapplication/RegisterFragment.kt
@@ -1,27 +1,58 @@
 package com.refathasan.myapplication
 
+import android.app.Activity
 import android.os.Bundle
+import android.text.TextUtils
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
+import androidx.appcompat.content.res.AppCompatResources
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [RegisterFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class RegisterFragment : Fragment() {
+    private lateinit var userName: EditText
+    private lateinit var password: EditText
+    private lateinit var confirmPassword: EditText
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_register, container, false)
+        var view = inflater.inflate(R.layout.fragment_register, container, false)
+        userName = view.findViewById(R.id.edt_user_name)
+        password = view.findViewById(R.id.edt_password)
+        confirmPassword = view.findViewById(R.id.edt_conf_password)
+        view.findViewById<Button>(R.id.btn_login).setOnClickListener {
+            var navLogin = activity as FragmentNavigationInterface
+            navLogin.navifateFragment(LoginFragment(), false)
+        }
+        view.findViewById<Button>(R.id.btn_register).setOnClickListener {
+            validateForm()
+        }
+        return view
     }
+
+    private fun validateForm() {
+        val icon = AppCompatResources.getDrawable(requireContext(), R.drawable.ic_alert)
+        icon?.setBounds(0, 0, icon.intrinsicWidth, icon.intrinsicHeight)
+        when {
+            TextUtils.isEmpty(userName.text.toString().trim()) -> {
+                userName.setError("Please Enter User Name", icon)
+            }
+
+            TextUtils.isEmpty(password.text.toString().trim()) -> {
+                password.setError("Please Enter Password", icon)
+            }
+
+            TextUtils.isEmpty(confirmPassword.text.toString().trim()) -> {
+                confirmPassword.setError("Please Enter Password Confirmation", icon)
+            }
+        }
+    }
+
 }
+
+
