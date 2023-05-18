f5f18cc47112eb797d5277377b15d1c047ac3bb1
refat hasan <refat.aiub@hotmail.com>
2023-05-18 13:21:43 +0530

    version 1.0.0.0 commit number 2.00 - Manual Product Added and tested

---
+++ app/src/main/java/com/refathasan/myapplication/ProductAdapter.kt
@@ -0,0 +1,52 @@
+package com.refathasan.myapplication
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+
+/**
+ *ProductAdapter class
+ */
+class ProductAdapter(private var productList:MutableList<Product>) : RecyclerView.Adapter<ProductAdapter.ProductViewHolder>() {
+
+    /**
+     * implemented methods
+     */
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ProductViewHolder {
+        val layoutView:View = LayoutInflater.from(parent.context).inflate(R.layout.product_page,parent,false)
+        return ProductViewHolder(layoutView)
+    }
+
+    /**
+     *implemented methods
+     */
+    override fun getItemCount(): Int {
+        /**
+         * Temporary number
+         */
+        return productList.size
+    }
+
+    /**
+     *implemented methods
+     */
+    override fun onBindViewHolder(holder: ProductViewHolder, position: Int) {
+        holder.productImage.setImageResource(productList[position].image)
+        holder.productTitle.setText(productList[position].title)
+        holder.productDetails.setText(productList[position].description)
+        holder.productPrice.setText(productList[position].price)
+    }
+
+    /**
+     * ViewHolder Class
+     */
+    class ProductViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+        var productImage:ImageView = view.findViewById(R.id.product_image)
+        var productTitle:TextView = view.findViewById(R.id.product_title)
+        var productDetails:TextView = view.findViewById(R.id.product_desc)
+        var productPrice:TextView = view.findViewById(R.id.product_price)
+    }
+}
--- app/src/main/java/com/refathasan/myapplication/RegisterFragment.kt
+++ app/src/main/java/com/refathasan/myapplication/RegisterFragment.kt
@@ -9,13 +9,18 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import android.widget.EditText
+import android.widget.Toast
 import androidx.appcompat.content.res.AppCompatResources
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 
 class RegisterFragment : Fragment() {
     private lateinit var userName: EditText
     private lateinit var password: EditText
     private lateinit var confirmPassword: EditText
+    private lateinit var firebaseAuthentication:FirebaseAuth
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -24,6 +29,7 @@ class RegisterFragment : Fragment() {
         var view = inflater.inflate(R.layout.fragment_register, container, false)
         userName = view.findViewById(R.id.edt_user_name)
         password = view.findViewById(R.id.edt_password)
+        firebaseAuthentication = Firebase.auth
         confirmPassword = view.findViewById(R.id.edt_conf_password)
         view.findViewById<Button>(R.id.btn_login).setOnClickListener {
             var navLogin = activity as FragmentNavigationInterface
@@ -35,6 +41,28 @@ class RegisterFragment : Fragment() {
         return view
     }
 
+    /**
+     * Firebase Registration method
+     */
+    private fun firebaseSingUp() {
+        firebaseAuthentication.createUserWithEmailAndPassword(userName.text.toString(),password.text.toString()).addOnCompleteListener {
+                task->
+            if(task.isSuccessful){
+                var homeFragment = activity as FragmentNavigationInterface
+                homeFragment.navifateFragment(HomeFragment(),true)
+                Toast.makeText(context, "Register Successful", Toast.LENGTH_SHORT)
+                    .show()
+            }else{
+
+                Toast.makeText(context, task.exception?.message, Toast.LENGTH_SHORT)
+                    .show()
+            }
+        }
+    }
+    /**
+     * validateForm method to check email, password
+     *
+     **/
     private fun validateForm() {
         val icon = AppCompatResources.getDrawable(requireContext(), R.drawable.ic_alert)
         icon?.setBounds(0, 0, icon.intrinsicWidth, icon.intrinsicHeight)
@@ -50,9 +78,28 @@ class RegisterFragment : Fragment() {
             TextUtils.isEmpty(confirmPassword.text.toString().trim()) -> {
                 confirmPassword.setError("Please Enter Password Confirmation", icon)
             }
+
+            userName.text.toString().isNotEmpty() && password.text.toString()
+                .isNotEmpty() && confirmPassword.text.isNotEmpty() -> {
+                //regex email = [a-zA-Z0-9._-]+@[a-z]+\\.+[a-z]+
+                if (userName.text.toString().matches(Regex("[a-zA-Z0-9._-]+@[a-z]+\\.+[a-z]+"))) {
+                    if (password.text.toString().length >= 5) {
+                        if (password.text.toString() == confirmPassword.text.toString()) {
+                            //temporary Toast message would be change in final version
+                            firebaseSingUp()
+
+                        } else {
+                            confirmPassword.setError("Password mismatched", icon)
+                        }
+                    } else {
+                        password.setError("Please Enter password at least 5 character long", icon)
+                    }
+                } else {
+                    userName.setError("Please Enter valid email-address", icon)
+                }
+            }
         }
     }
-
 }
 
 
