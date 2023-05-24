69ddb82ad060a603f3bc72ceeeccc56fe11f552c
refat hasan <refat.aiub@hotmail.com>
2023-05-24 07:48:29 +0530

    version 1.0.0.0 commit number 2.50 - Product Added and tested from Database

--- app/src/main/java/com/refathasan/myapplication/Product.kt
+++ app/src/main/java/com/refathasan/myapplication/Product.kt
@@ -1,5 +1,5 @@
 package com.refathasan.myapplication
 
-class Product(var image:Int, var title:String, var description:String, var price:String) {
+class Product(var image:String, var title:String, var description:String, var price:String) {
 
 }
--- app/src/main/java/com/refathasan/myapplication/ProductAdapter.kt
+++ app/src/main/java/com/refathasan/myapplication/ProductAdapter.kt
@@ -6,17 +6,20 @@ import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.squareup.picasso.Picasso
 
 /**
  *ProductAdapter class
  */
-class ProductAdapter(private var productList:MutableList<Product>) : RecyclerView.Adapter<ProductAdapter.ProductViewHolder>() {
+class ProductAdapter(private var productList: MutableList<Product>) :
+    RecyclerView.Adapter<ProductAdapter.ProductViewHolder>() {
 
     /**
      * implemented methods
      */
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ProductViewHolder {
-        val layoutView:View = LayoutInflater.from(parent.context).inflate(R.layout.product_page,parent,false)
+        val layoutView: View =
+            LayoutInflater.from(parent.context).inflate(R.layout.product_page, parent, false)
         return ProductViewHolder(layoutView)
     }
 
@@ -34,19 +37,24 @@ class ProductAdapter(private var productList:MutableList<Product>) : RecyclerVie
      *implemented methods
      */
     override fun onBindViewHolder(holder: ProductViewHolder, position: Int) {
-        holder.productImage.setImageResource(productList[position].image)
+        Picasso.get()
+            .load(productList[position].image)
+            .into(holder.productImage)
+        //Picasso.get()
+          //  .load("https://firebasestorage.googleapis.com/v0/b/shopeassy-2e18b.appspot.com/o/parle_g.jpeg?alt=media&token=2248773c-db0a-4a41-a64f-158988c37494")
+        //holder.productImage.setImageResource(productList[position].image)
         holder.productTitle.setText(productList[position].title)
         holder.productDetails.setText(productList[position].description)
-        holder.productPrice.setText(productList[position].price)
+        holder.productPrice.setText("$ "+productList[position].price)
     }
 
     /**
      * ViewHolder Class
      */
     class ProductViewHolder(view: View) : RecyclerView.ViewHolder(view) {
-        var productImage:ImageView = view.findViewById(R.id.product_image)
-        var productTitle:TextView = view.findViewById(R.id.product_title)
-        var productDetails:TextView = view.findViewById(R.id.product_desc)
-        var productPrice:TextView = view.findViewById(R.id.product_price)
+        var productImage: ImageView = view.findViewById(R.id.product_image)
+        var productTitle: TextView = view.findViewById(R.id.product_title)
+        var productDetails: TextView = view.findViewById(R.id.product_desc)
+        var productPrice: TextView = view.findViewById(R.id.product_price)
     }
 }
